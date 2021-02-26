38285c6c3b4b7a62353caeef279cf2cc1b3ba7eb
cutieie <79693212+cutieie@users.noreply.github.com>
2021-02-26 17:30:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-# finalpt
+<html>
+<head> <title> FINAL PT </title>
+<body bgcolor= "#DEB887"> 
+</head>
+<body>
+<font face= "times new roman", color= "black", size= "15">
+<marquee direction= "right"> I'm Breaking up with covid-19 </marquee></font>
+<font face= "times new roman", color= "#005baa", size= "15">
+<marquee> I need hope </marquee></font>
+<center>
+ <audio controls>
+  
+  <source src="BTS (방탄소년단) - Life Goes On Piano Cover.ogg" type="audio/ogg">
+  <source src="BTS (방탄소년단) - Life Goes On Piano Cover.mp3" type="audio/mpeg"> 
+ </audio>
+
+<font face= "courier", color= "#0031aa", size= "7">
+<h1> Today </h1>
+</font>
+<font face= "optima", color= "2F4F4F", size= "4">
+<b>The dilemna I've had since Covid-19 entered my life will come to an end.</b><br>
+<b>Thought it was happiness, but it was not.</b><br> 
+<b>Yet, I'm glad it came to my life for I had realized a lot. But now I'm letting go.</b><br>
+<b> I'm moving on. I want my life to be normal. I want everyone to live normally again and Covid-19 to leave us all alone.</b><br><br><br>
+<img src= "covid.jpg", length= "536", width= "676"><br><br><br>
+<b> This year, I'm hoping for something else to arrive. A pure bag of happiness and a sack of hope. </b><br>
+<b>I want to see people having hope again and the World to regain it's strength and to be cured.</b><br>
+<b>Covid-19 had been somehow great and had made miserable things in my life, in everyones lives.</b><br><br><br>
+<i> But, I want all of you to know that still; Life Goes On </i><br><br> 
+</font>
+
+<font face= "consolas", color= "274283", size= "6">
+<b> Like an echo in the forest</b><br>
+<b> The day will come back around </b><br>
+<b> As if nothing happened </b><br>
+<b> Yeah, life goes on</b><br>
+<i> -BTS "Life Goes On" </i> <br><br><br><br> 
+</font>
